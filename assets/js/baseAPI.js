cb24706ffed224e0d7f2759d8f497d1c6f9935b6
Rogers <raochen960822@gmail.com>
2021-05-31 11:41:26 +0800

    完成了index页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,17 @@
 $.ajaxPrefilter(function (option) {
+    /* 统一为API接口拼接根路径 */
     option.url = 'http://api-breakingnews-web.itheima.net' + option.url;
+    /* 为有权限的请求添加Headers */
+    if (option.url.includes('/my/')) {
+        option.headers = {
+            Authorization: localStorage.token || ' ',
+        }
+    }
+    /* 统一为 */
+    option.complete = function (result) {
+        if (result.responseJSON.status === 1 && result.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+$(function () {
+    getUserInfo();
+    $('#btn-log-out').click(function () {
+        layer.confirm('是否退出？', { icon: 3, title: '提示' }, function (index) {
+            location.href = '/login.html';
+            localStorage.removeItem('token');
+            layer.close();
+        })
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (result) {
+            console.log(result);
+            if (result.status !== 0) return layui.layer.msg('获取用户信息失败!');
+            renderAvatar(result.data);
+        }
+    })
+}
+function renderAvatar(data) {
+    var name = data.nickname || data.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp' + name);
+    if (data.user_pic !== null) {
+        $('.layui-nav-img').attr('src', data.user_pic).show().siblings('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide().siblings('.text-avatar').html(name[0].toUpperCase()).show();
+    }
+}
